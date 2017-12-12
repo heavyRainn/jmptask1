81ed13270141589cc1dd22b29fd026d0e7da193b
Andrew <fvigo@mail.ru>
2017-12-12 21:21:36 +0300

    refactoring

--- src/main/java/com/jmp/epam/one/main/Main.java
+++ src/main/java/com/jmp/epam/one/main/Main.java
@@ -5,16 +5,10 @@ import com.jmp.epam.one.indexer.core.Indexer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
-
-    private static final String FILENAME = "indexation.txt";
 
     public static void main(String[] args) {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
@@ -22,9 +16,6 @@ public class Main {
 
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.submit(indexer);
-
-//        String content = new String(Files.readAllBytes(Paths.get(FILENAME)));
-//        System.out.println(content);
     }
 
 }
